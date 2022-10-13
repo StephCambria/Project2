0ec9b6a34cf7dad663f8b945bd5e3f381203206a
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-13 16:53:27 -0400

    base server set up
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,52 @@
+// TODO: install the following
+
+// sequelize || npm i sequelize
+// mysql2 || npm i mysql2
+// express || npm i express
+// express-handlebars || npm i express-handlebars
+// express-session || npm i express-session
+// connect-session-sequelize || npm i connect-session-sequelize
+// dotenv || npm i dotenv
+// bcrypt || npm i bcrypt
+
+// ------------------------------------------
+
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 9999;
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening..."));
+});
