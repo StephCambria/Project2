3b1ae3f60f3a95ad749d0a546823dbeb82d25022
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-19 19:11:28 -0400

    js / views progress
    login / logout / signup js (hopefully) solid, making sure ids, classes, and links are named correctly in each file
    
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,19 +1,18 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
       
-        const name = document.querySelector('#name-signup').value.trim();
-        const email = document.querySelector('#email-signup').value.trim();
+        const username = document.querySelector('#username-signup').value.trim();
         const password = document.querySelector('#password-signup').value.trim();
       
-        if (name && email && password) {
-          const response = await fetch('/api/users', {
+        if (username && password) {
+          const response = await fetch('/api/user', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
           });
       
           if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/grocery');
           } else {
             alert(response.statusText);
           }
@@ -21,5 +20,5 @@ const signupFormHandler = async (event) => {
       };
   
       document
-      .querySelector('.signup-form')
+      .querySelector('#signup-btn')
       .addEventListener('submit', signupFormHandler);
