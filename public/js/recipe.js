989b33742058599540ca2cb439254a4bd5a98eb6
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-24 18:12:31 -0400

    API-related shenanigans
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,1 +1,28 @@
-const recipeApi = 
+var repoList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  
+  fetch("https://api.spoonacular.com/recipes/random?apiKey=1087b1d9dd91449b9315698f7cc25850&number=1&tags=vegetarian,dessert")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.recipes[0].title);
+      console.log(data.recipes[0]);
+  
+        var image = data.recipes[0].image;
+        var title = data.recipes[0].title;
+        var spoonacularSourceUrl = data.recipes[0].spoonacularSourceUrl;
+        
+        document.querySelector(".image").src = image;
+        document.querySelector(".title").innerText = title;
+        document.querySelector(".spoonacularSourceUrl").href = spoonacularSourceUrl;
+
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+}
+
+fetchButton.addEventListener('click', getApi);
