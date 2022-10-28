217f108fe10a33cfdd4c7c04824b25a7dd283f62
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-28 18:06:30 -0400

    logout button no longer renders + I don't know why
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- public/js/logout.js
+++ public/js/logout.js
@@ -7,9 +7,8 @@ const logout = async () => {
     if (response.ok) {
       document.location.replace('/');
     } else {
-      alert('Failed to log out.');
+      alert(response.statusText);
     }
   };
   
-  document.querySelector('#logout-link').addEventListener('click', logout);
-  
+document.querySelector('#logout-link').addEventListener('click', logout);
