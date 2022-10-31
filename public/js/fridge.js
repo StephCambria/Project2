2e74f3ab8cb97f57ba03237e84dcd9c6c80b2d95
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-10-31 17:53:47 -0400

    rearranging changes
    Signed-off-by: Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>

--- public/js/fridge.js
+++ public/js/fridge.js
@@ -1,7 +1,7 @@
 addItem = () => {
 
     // Get type of element from form
-    // let type = document.getElementById("type").value;
+    //let type = document.getElementById("type").value;
     
     // Get the text/value for the tag
     // from the form
